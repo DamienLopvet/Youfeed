0a939305f1af6ad9e8d9d64895c1c76566db943e
diaman t <damienlopvet@gmail.com>
2022-06-18 15:03:38 +0200

    add feed gap

--- src/components/Youfeed.vue
+++ src/components/Youfeed.vue
@@ -1,24 +1,20 @@
 <template>
   <section>
-    <div class="d-flex flex-row justify-content-around">
+    <div class="d-flex flex-row justify-content-around py-3" >
       <button class="btn btn-outline-warning mb-2" @click="addFeed(e = 'left')">tétée gauche</button>
       <button class="btn btn-outline-success mb-2" @click="addFeed(e = 'right')">tétée droite</button>
     </div>
-    <div class="container">
-      <div class="row">
 
-        <div class="col">
-          <div v-for="(feed, index) in feeds" :key="index">
-            <div class="list-group-item list-group-item-success text-primary mb-1" :class="feed.side=='left'?'left':'right'">{{feed.date}}
-              <small><button class="btn btn-outline-danger " @click="removeFeed(index)">
-                <i class="fa-solid fa-trash-can cursor-pointer"></i>
-              </button></small>
-            </div>
-          </div>
+    <div class="col">
+      <div v-for="(feed, index) in feeds" :key="index">
+        <div class="pt-3 pb-0 list-group-item list-group-item-success text-primary mb-1 d-flex flex-row flex-wrap justify-content-between align-items-baseline"
+          :class="feed.side=='left'?'left':'right'">
+          <p>{{new Date(feed.date).toLocaleTimeString([], { weekday: 'long', month: 'long', day: 'numeric',hour: '2-digit', minute: '2-digit' })}}</p>
+          <span v-if="feed.gap" class="text-dark shadow-sm align-self-end feed-gap mb-1">+ {{ new Date(feed.gap).toISOString().slice(11, -8) }}</span>
+          <button class="btn btn-outline-danger ml-auto " @click="removeFeed(index)">
+            <i class="fa-solid fa-trash-can cursor-pointer"></i>
+          </button>
         </div>
-
-
-
       </div>
     </div>
   </section>
@@ -38,10 +34,13 @@
         this.feeds = JSON.parse(localStorage.getItem("feeds")) || [];
       },
       addFeed(e) {
-        
+        let lastFeed =  this.feeds[0]?.date
+        let feedGap =  Date.now()-  parseInt(lastFeed) 
         let newFeed = {
-          date: new Date().toLocaleTimeString([], { weekday: 'long', month: 'long', day: 'numeric' }),
-          side: e
+          date: Date.now(),
+          side: e,
+          gap: feedGap
+
         }
         this.feeds.unshift(newFeed);
 
@@ -62,32 +61,40 @@
       },
 
     },
+    
     mounted() {
       this.setFeeds()
     }
   }
 </script>
 <style>
-  .left:before{
-    content:'';
-    width:50%;
-    height:10%;
-    background-color: #ffc107!important;
+  .left:before {
+    content: '';
+    width: 50%;
+    height: 10%;
+    background-color: #ffc107 !important;
     position: absolute;
     top: 0;
-    left:-1px;
-    border-bottom-right-radius:30px
-
+    left: -1px;
+    border-bottom-right-radius: 30px
   }
-  .right:before{
-    content:'';
-    width:50%;
-    height:10%;
-    background-color: #198754!important;
+
+  .right:before {
+    content: '';
+    width: 50%;
+    height: 10%;
+    background-color: #198754 !important;
     position: absolute;
     top: 0;
-    right:-1px;
-    border-bottom-left-radius:30px
-
+    right: -1px;
+    border-bottom-left-radius: 30px
   }
+  .feed-gap{
+    font-size: 9px;
+    border:1px solid black;
+    padding: 0 5px;
+    border-top-left-radius: 5px;
+    border-radius: 157px 55px / 168px;
+    
+}
 </style>
